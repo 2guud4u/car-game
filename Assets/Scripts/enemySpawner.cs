0b17bb4f8dba03d52e0d3db4f9679a7c808adba1
Jiahua Jiang <jiahua212@gmail.com>
2024-02-15 09:54:01 -0800

    added spawned

---
+++ Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float spawnRate = 5.0f;
+    public GameObject enemyPrefab;
+    // Update is called once per frame
+    //public AudioSource audioSource;
+    void Start()
+    {
+        StartCoroutine(SpawnWithDelayCoroutine());
+    }
+    IEnumerator SpawnWithDelayCoroutine(){
+        while(true){
+            yield return new WaitForSeconds(spawnRate);
+            spawnEnemy();
+            //audioSource.Play();
+        }
+        
+        
+       
+    }
+    private void spawnEnemy(){
+        // spawning left
+       
+        Vector3 spawnPos = new Vector3(Random.Range(-4,33), 1.5f, Random.Range(50, 10));
+        GameObject l = Instantiate(enemyPrefab, spawnPos, Quaternion.identity); 
+            
+
+        
+    }
+}
+
