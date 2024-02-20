778ccf67eed938b4c600ccaf5d2851fa1eb5b00c
Jiahua Jiang <jiahua212@gmail.com>
2024-02-20 08:28:59 -0800

    soldier strike delay

--- Assets/Scripts/soldierScript.cs
+++ Assets/Scripts/soldierScript.cs
@@ -1,7 +1,7 @@
 
 using UnityEngine;
 using UnityEngine.AI;
-
+using System.Collections;
 public class soldierScript : enemyScript
 {   
     public override void AttackPlayer()
@@ -13,16 +13,18 @@ public class soldierScript : enemyScript
 
         if (!alreadyAttacked)
         {
-            ///Attack code here
-            Rigidbody rb = Instantiate(projectile, transform.position + transform.forward * 2, Quaternion.identity).GetComponent<Rigidbody>();
-            // rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            // rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-            ///End of attack code
+            StartCoroutine(ActivateAttack(1.0f));
+                      
 
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
-        //turnOnRagdoll();
         
+        
+    }
+    IEnumerator ActivateAttack(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Rigidbody rb = Instantiate(projectile, transform.position + transform.forward * 2, Quaternion.identity).GetComponent<Rigidbody>();
     }
 }
