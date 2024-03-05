10e057ac9cb2d71d2ff2058348c6f1ade725b182
a-cherian <akashccherian@gmail.com>
2024-03-04 16:15:35 -0800

    Walls burst when you hit them fast enough

---
+++ Assets/Scripts/Building/BurstOnCollide.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BurstOnCollide : MonoBehaviour
+{
+    public AudioClip collisionSound;
+    public GameObject particles;
+    public float volume;
+    [SerializeField] string[] collisionTags;
+    int burstThreshold = 25;
+
+    AudioSource audioSource;
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if(collision.relativeVelocity.magnitude > burstThreshold && collisionTags.Any(tag => other.CompareTag(tag)))
+        {
+            audioSource.PlayOneShot(collisionSound, volume);
+            Rigidbody[] rbs = transform.parent.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rb in rbs)
+            {
+                rb.isKinematic = false;
+                rb.AddForce(collision.relativeVelocity * 20, ForceMode.Impulse);
+            }
+            Instantiate(particles, collision.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        GameObject other = collision.gameObject;
+        Rigidbody collisionBody = collision.attachedRigidbody;
+        if(collisionBody == null) { return; }
+        if(collisionBody.velocity.magnitude > burstThreshold && collisionTags.Any(tag => other.CompareTag(tag)))
+        {
+            audioSource.PlayOneShot(collisionSound, volume);
+            Instantiate(particles, collision.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Rigidbody[] rbs = transform.parent.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rb in rbs)
+            {
+                rb.isKinematic = false;
+                rb.AddForce(collisionBody.velocity * 200, ForceMode.Impulse);
+            }
+        }
+    }
+}
