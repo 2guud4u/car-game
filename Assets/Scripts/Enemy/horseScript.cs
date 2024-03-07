8f1f3f62186594cab097d04627fffa7ee29ab9aa
Jiahua Jiang <jiahua212@gmail.com>
2024-03-07 11:38:59 -0800

    horse

---
+++ Assets/Scripts/Enemy/horseScript.cs
@@ -0,0 +1,46 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+public class horseScript : enemyScript
+{   
+    float timeSwordIsDrawn = 0.7f;
+
+    public override void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack code here
+           
+            DrawSword();
+
+            alreadyAttacked = true;
+            
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+        
+        
+    }
+
+    public void DrawSword()
+    {
+        projectile.tag = "Weapon";
+        // projectile.SetActive(true);
+        Invoke(nameof(HideSword), timeSwordIsDrawn);
+    }
+
+    public void HideSword()
+    {
+        projectile.tag = "Untagged";
+        // projectile.SetActive(false);
+    }
+
+    void OnDestroy() {
+        Destroy(projectile);
+    }
+}
